17e3e05111bb51a490f238ca34482654f7e6a1e1
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-15 13:45:25 +0530

    Add: total amount after 20 days

--- gambler.java
+++ gambler.java
@@ -1,23 +1,43 @@
-package Bridgelab.gambling;
+package com.bridgelab.gambling;
 public class GamblerProblem {
-	//Stake of $100 and every bet is of $1
+	// UC-1 Stake of $100 and bet of $1
 	public static final int STAKE = 100;
 	public static final int BET = 1;
-		
-	//Method to determine win or loss
-	static void winOrLoss(){
-		double gambling = Math.floor(Math.random() * 10) % 2;
-		if(gambling == 1) {
-			System.out.println("Win");
+
+	// UC-2 Win or lose method
+	public void winOrLoss() {
+		int totalAmt = STAKE;
+
+		// UC-4 Gambling for 20 days
+		for (int days = 0; days < 20; days++) {
+
+			// UC-3 Gambling till 50% of stake is lost or won
+			int stake = STAKE;
+			int target = 150;
+			int loss = 50;
+			while ((stake < target && stake > loss)) {
+				double random = Math.floor(Math.random() * 10) % 2;
+				if (random == 1) {
+					totalAmt += BET;
+					stake++;
+				} else {
+					totalAmt -= BET;
+					stake--;
+				}
+			}
+			if (stake == 50) {
+				System.out.println("gambler lost 50% of the stake");
+			} else {
+				System.out.println("gambler won 50% of the stake");
+			}
+			System.out.println(stake);
 		}
-		else {
-			System.out.println("loss");
-		}
+
+		System.out.println("total stake after 20 days " + totalAmt);
 	}
-		
+
 	public static void main(String args[]) {
-		System.out.println("Welcome to the java gambler problem");
-		winOrLoss();	
-		}	
-	
+		GamblerProblem gambler = new GamblerProblem();
+		gambler.winOrLoss();
 	}
+}
