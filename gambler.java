9a3f9676bf14e08e4ae07a993dd07b042cb883e2
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-15 10:47:36 +0530

    Add: stake and bet

--- gambler.java
+++ gambler.java
@@ -3,6 +3,8 @@ package Bridgelab.gambling;
 public class GamblerProblem {
 	public static void main(String args[]) {
 		System.out.println("Welcome to the java gambler problem");
+		 final int STAKE = 100;
+		 final int BET = 1;
 	}
 
 }
