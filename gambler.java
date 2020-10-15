d0ad37e4ccbd58e6bc302c710ec44f4fb4a3f5f9
Shreya24byte <shreyanavelkar24@gmail.com>
2020-10-15 10:52:15 +0530

    Add: win or loss

--- gambler.java
+++ gambler.java
@@ -1,10 +1,23 @@
 package Bridgelab.gambling;
-
 public class GamblerProblem {
+	//Stake of $100 and every bet is of $1
+	public static final int STAKE = 100;
+	public static final int BET = 1;
+		
+	//Method to determine win or loss
+	static void winOrLoss(){
+		double gambling = Math.floor(Math.random() * 10) % 2;
+		if(gambling == 1) {
+			System.out.println("Win");
+		}
+		else {
+			System.out.println("loss");
+		}
+	}
+		
 	public static void main(String args[]) {
 		System.out.println("Welcome to the java gambler problem");
-		 final int STAKE = 100;
-		 final int BET = 1;
+		winOrLoss();	
+		}	
+	
 	}
-
-}
